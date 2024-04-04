b98c08c6eedd9f26cf5b20ab8d74f9aa92ed8b32
zikriaulia28 <zikriaulia98@gmail.com>
2024-04-04 19:56:35 +0700

    first completed - ui design

---
+++ src/components/messages/Messages.jsx
@@ -0,0 +1,50 @@
+import Message from "./Message";
+
+const Messages = () => {
+  return (
+    <div className="px-4 flex-1 overflow-auto">
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+    </div>
+  );
+};
+
+export default Messages;
+
+//STARTED CODE
+// import Message from "./Message";
+
+// const Messages = () => {
+//   return (
+//     <div className="px-4 flex-1 overflow-auto">
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//     </div>
+//   );
+// };
+
+// export default Messages;
